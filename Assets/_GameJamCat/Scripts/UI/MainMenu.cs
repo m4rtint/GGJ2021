9016836fe89bad817cd794a3cbc3386f605cc94e
Gursaanj <gursaanjresides@gmail.com>
2021-01-31 13:23:29 -0800

    Working MainMenu

---
+++ Assets/_GameJamCat/Scripts/UI/MainMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace GameJamCat
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button _startGameButton = null;
+        [SerializeField] private Button _quitGameButton = null;
+
+        private void Awake()
+        {
+            if (_startGameButton != null)
+            {
+                _startGameButton.onClick.AddListener(() => SceneManager.LoadScene("MainScene"));
+            }
+
+            if (_quitGameButton != null)
+            {
+                _quitGameButton.onClick.AddListener(() => Application.Quit());
+            }
+        }
+    }
+}
