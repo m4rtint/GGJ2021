3ce773c40ba2e814338534246947541834025176
Gursaanj <gursaanjresides@gmail.com>
2021-01-31 16:07:45 -0800

    Fixed Merge Conflicts

--- Assets/_GameJamCat/Scripts/CatBehaviour.cs
+++ Assets/_GameJamCat/Scripts/CatBehaviour.cs
@@ -1,9 +1,7 @@
 using Cinemachine;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using Random = UnityEngine.Random;
 
 
 namespace GameJamCat
